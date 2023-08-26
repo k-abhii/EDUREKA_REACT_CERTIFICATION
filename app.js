a2f4ab1c0110df1636f992b0e23148c46cc5105f
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-08-26 22:12:40 +0530

    Calculator with ES5

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const sum = require('./sum');
-const sub = require('./sub')
-console.log(sum(4,5));
-console.log(sub(4,5));
+// const sum = require('./sum');
+// const sub = require('./sub')
+// console.log(sum(4,5));
+// console.log(sub(4,5));
+
+const calcy = require("./calculator")
+console.log(calcy.sum(40,50));
+console.log(calcy.sub(40,50));
+console.log(calcy.mul(40,50));
+console.log(calcy.div(40,50));
---
+++ calculator.js
@@ -0,0 +1,18 @@
+const calculator ={
+    sum: function (n1,n2){
+        return n1+n2;
+    },
+    sub:function(n1,n2){
+        return n1-n2;
+    },
+    mul:function (n1,n2){
+        return n1*n2
+
+    },
+    div:function(n1,n2){
+        return n1/n2
+    },
+
+}
+
+module.exports=calculator;
